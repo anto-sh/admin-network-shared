fb6fdb98032a7a7aa69d39878e0fe5ab4957dc98
antosh <tomatomoving@mail.ru>
2026-06-18 17:40:54 +0300

    feat: added build step

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./network-message-codes";
-export * from "./network-message-params";
-export * from "./network-api-response";
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./network-message-codes.ts";
+export * from "./network-message-params.ts";
+export * from "./network-api-response.ts";
